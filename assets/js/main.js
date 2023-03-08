baa8bb32eb8d8296adb71d8755421fe913b05a67
DamianoCasolari <damianocasolari@gmail.com>
2023-03-08 16:36:24 +0100

    add 3 const + result

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,28 @@
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 
-const age = prompt();
-console.log(Number(age).toFixed(2))
+const age = Number(prompt('inserisci la tua età'));
+console.log(age);
+
+const km = Number(prompt('inserisci i kilometri che vuoi percorrere'));
+console.log(km);
+
+const km_price = km * 0.21;
+console.log(km_price);
+
+let discounted_price;
+if (age < 18) {
+    discounted_price = km_price / 100 * 80 ;
+} else if (age > 65 ){
+    discounted_price = km_price / 100 * 60 ;
+} else {
+    discounted_price = km_price;
+}
+
+let discounted_price_with_decimal = discounted_price.toFixed(2);
+
+console.log(`il costo del biglietto ammonta a ${discounted_price_with_decimal} euro`);
+
+
 
 
