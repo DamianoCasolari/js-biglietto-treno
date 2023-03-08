a2ef7e1c1d9661cdfd9ae98ca3d67bf7aebe7944
DamianoCasolari <damianocasolari@gmail.com>
2023-03-08 20:15:57 +0100

    Custom layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,19 +6,26 @@
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 
-const age = Number(prompt('inserisci la tua età'));
-console.log(age);
+// const age = Number(prompt('inserisci la tua età'));
+// console.log(age);
+// const km = Number(prompt('inserisci i kilometri che vuoi percorrere'));
+// console.log(km);
 
-const km = Number(prompt('inserisci i kilometri che vuoi percorrere'));
-console.log(km);
+let age = document.querySelector(".age_input");
 
-const km_price = km * 0.21;
+
+let km = document.querySelector(".km_input");
+
+
+function clickOnTest(event){
+
+    let km_price = km.value * 0.21;
 console.log(km_price);
 
 let discounted_price;
-if (age < 18) {
+if (age.value < 18) {
     discounted_price = km_price / 100 * 80 ;
-} else if (age > 65 ){
+} else if (age.value > 65 ){
     discounted_price = km_price / 100 * 60 ;
 } else {
     discounted_price = km_price;
@@ -29,6 +36,16 @@ let discounted_price_with_decimal = discounted_price.toFixed(2);
 console.log(`il costo del biglietto ammonta a ${discounted_price_with_decimal} euro`);
 
 document.getElementById("ticket_price").innerHTML = `Il costo del biglietto ammonta a ${discounted_price_with_decimal} euro`;
+
+}
+// ottiene l'elemento 'test'
+const click_for_ticket = document.querySelector('.btn');
+// associa l'evento click alla callback
+click_for_ticket.addEventListener('click', clickOnTest);
+
+
+
+
 
 
 
